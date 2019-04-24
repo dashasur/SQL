d72477f359a8b001367b4c40947b2dcf0ec5bfc8
dashasur <surtaevad@gmail.com>
2019-04-24 16:35:19 +1200

    Для каждого значения скорости ПК, превышающего 600 МГц, определите среднюю цену ПК с такой же скоростью. Вывести: speed, средняя цена.

--- sql_tasks.sql
+++ sql_tasks.sql
@@ -46,3 +46,11 @@ SELECT maker, MAX(price)
 FROM PC,Product
 Where PC.model=Product.model
 Group BY maker
+
+
+--#22 Для каждого значения скорости ПК, превышающего 600 МГц, определите среднюю цену ПК с такой же скоростью. Вывести: speed, средняя цена.
+SELECT speed,
+       Avg(price) AS Avg_price
+FROM   pc
+WHERE  speed > 600
+GROUP  BY speed  
